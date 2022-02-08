021017984573a2c24b2d82786c80f1d02b686bc2
Maks Fedorenchik <maksfedorenchik@gmail.com>
2022-02-08 18:03:32 +0200

    small fix

--- src/components/InvotraTooltip/presets/0-default.jsx
+++ src/components/InvotraTooltip/presets/0-default.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { IconButton } from '@mui/material';
-import * as Icons from '@mui/icons-material';
 import InvotraTooltip from '../InvotraTooltip';
 
 export default (
   <InvotraTooltip uxpId="invotra-tooltip" title={'Merge'}>
     <IconButton color={'inherit'}>
-      <Icons.ModeOutlined />
+      home
     </IconButton>
   </InvotraTooltip>
 );
