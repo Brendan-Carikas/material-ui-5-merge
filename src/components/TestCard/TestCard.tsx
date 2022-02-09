26bc7a92f859a8471461509243db35e33bee759b
Maks Fedorenchik <maksfedorenchik@gmail.com>
2022-02-09 13:53:26 +0200

    fix TestCard Component

--- src/components/TestCard/TestCard.tsx
+++ src/components/TestCard/TestCard.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import MUICard from '@mui/material/Card';
 import { CardProps } from '@mui/material/Card';
 
-export default function TestCard(props: CardProps) {
+interface TestCardProps {
+  children: CardProps["children"];
+  classes: CardProps["classes"];
+  raised: CardProps["raised"];
+  sx: CardProps["sx"];
+}
+
+export default function TestCard(props: TestCardProps) {
   return (
     <MUICard {...props}>{props.children}</MUICard>
   )
