37481b4392412bd902f7eed53c7530ed5f3ace72
Maks Fedorenchik <maksfedorenchik@gmail.com>
2022-02-17 11:36:29 +0200

    small fixes for DataGrid component

--- src/components/DataGrid/DataGrid.js
+++ src/components/DataGrid/DataGrid.js
@@ -3,9 +3,16 @@ import { DataGrid as MUIDataGrid } from '@mui/x-data-grid';
 import PropTypes from 'prop-types';
 
 
-function DataGrid(props) {
+function DataGrid({rows, columns, pageSize, rowsPerPageOptions, checkboxSelection, disableSelectionOnClick}) {
   return (
-    <MUIDataGrid {...props} />
+    <MUIDataGrid
+      rows={rows}
+      columns={columns}
+      pageSize={pageSize}
+      rowsPerPageOptions={rowsPerPageOptions}
+      checkboxSelection={checkboxSelection}
+      disableSelectionOnClick={disableSelectionOnClick}
+    />
   )
 }
 
