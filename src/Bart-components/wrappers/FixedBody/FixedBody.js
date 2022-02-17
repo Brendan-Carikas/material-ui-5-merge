0f3b785368a65d7455cf74cbaabf596819a15b16
Maks Fedorenchik <maksfedorenchik@gmail.com>
2022-02-17 10:22:31 +0200

    fix FixedBody component

--- src/Bart-components/wrappers/FixedBody/FixedBody.js
+++ src/Bart-components/wrappers/FixedBody/FixedBody.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { default as FixedBodyExample } from '../../../Bart-components/FixedBody/FixedBody';
 
-function FixedBody(props) {
+function FixedBody(props, uxpinRef) {
     return (
       <FixedBodyExample
+        ref={uxpinRef}
         hasHeader={props.hasHeader}
         className={props.className}
       >
